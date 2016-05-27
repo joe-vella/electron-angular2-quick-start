4bf2f3bd764d45f4043dfcaf2926992db9f99ade
Joe Vella <joe.vella@divium.com>
2016-05-27 11:55:44 -0400

    Filtering js and map with gitignore

--- app/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var core_1 = require('@angular/core');
-var router_deprecated_1 = require('@angular/router-deprecated');
-require('rxjs/Rx');
-var app_component_1 = require('./app.component');
-core_1.enableProdMode();
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [router_deprecated_1.ROUTER_PROVIDERS]);
-//# sourceMappingURL=index.js.map
